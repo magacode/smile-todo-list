2320991beb3ea6bb242468d928d79afdb9350224
Magomed <magacode@gmail.com>
2020-07-19 10:02:19 +0300

    Tied all components through redux

---
+++ src/app/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import classes from './app.module.scss';
+
+const App = () => {
+    return <div>Hello</div>
+}
+
+export default App;
--- src/reset-password-page/reset-password-page.js
+++ src/reset-password-page/reset-password-page.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
+import ContainerRow from '../components/container-row';
+
 import classes from './reset-password-page.module.scss';
 
 const ResetPasswordPage = () => {
-    return 'Reset'
+    return (
+        <ContainerRow>
+            <div className="col">
+                <h1 className="mt-5 mb-4 text-center title-component">
+                    Восстановить пароль
+                </h1>
+                <div className="card mt-5 wrapper-component">
+                    <div className="card-body">
+                        <form>
+                            <div className="form-group">
+                                <label htmlFor="exampleInputEmail1">Email</label>
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="exampleInputEmail1"
+                                    aria-describedby="emailHelp"
+                                />
+                                <small id="emailHelp" className="form-text text-muted">
+                                    Введите свою электронную почту
+                                </small>
+                            </div>
+                            <button type="submit" className="btn btn-primary">
+                                Восстановить
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </ContainerRow>
+    )
 }
 
 export default ResetPasswordPage;
